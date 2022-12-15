b24acf3c2930a47c20efee1079640999f2b6cbe6
ZoOL <zhooul@gmail.com>
2022-12-15 10:15:53 +0800

    add bevy plugin and egui plugin

--- mofang/src/main.rs
+++ mofang/src/main.rs
@@ -1,3 +1,21 @@
+use bevy::prelude::*;
+use bevy_egui::{egui, EguiContext, EguiPlugin};
+
 fn main() {
-    println!("Hello, world!");
+    let mut app = App::new();
+    app.add_plugins(DefaultPlugins.set(WindowPlugin {
+        window: WindowDescriptor {
+            title: "Rubiks's Cube".to_string(),
+            ..default()
+        },
+        ..default()
+    }))
+    .add_plugin(EguiPlugin);
+
+    #[cfg(feature = "debug")]
+    {
+        app.add_plugin(bevy_inspector_egui::WorldInspectorPlugin::new());
+    }
+
+    app.run();
 }
